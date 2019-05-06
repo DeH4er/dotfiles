6fd6eaf72a01c2f5a251fbef577be54a5a092f99
Denys Lytviak <denys.lytviak@embiq.com>
2019-05-06 10:11:49 +0200

    fixes

--- dwm/install.sh
+++ dwm/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git clone git@github.com:DeH4er/dwm.git $HOME/source
+git clone git@github.com:DeH4er/dwm.git $HOME/source/dwm
 cd $HOME/source/dwm
 sudo make clean install
 cd $DOTFILE_PWD
--- st/install.sh
+++ st/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git clone git@github.com:DeH4er/st.git $HOME/source
+git clone git@github.com:DeH4er/st.git $HOME/source/st
 cd $HOME/source/st
 sudo make clean install
 cd $DOTFILE_PWD
