cee0616ad3b5b541e06745a640bb1e0120e1b219
Denys Lytviak <deh4er@gmail.com>
2020-07-21 20:41:31 +0000

    dont change sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,6 +5,6 @@ sudo pacman -S zsh
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-chsh -s zsh
+# chsh -s zsh
 ln -s -f $DOTFILE_PWD/zsh/.zshrc $HOME/.zshrc
 touch $HOME/.local_zshrc
