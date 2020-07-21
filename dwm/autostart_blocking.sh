8487bea9a60f5f74f011ef809feb839e47212841
Denys Lytviak <deh4er@gmail.com>
2020-07-21 18:33:21 +0000

    add scripts

---
+++ dwm/autostart_blocking.sh
@@ -0,0 +1,3 @@
+xset r rate 200 20
+setxkbmap -option caps:super
+setxkbmap -layout dvorak
--- dwm/install.sh
+++ dwm/install.sh
@@ -6,4 +6,10 @@ cd $HOME/source/dwm
 sudo make clean install
 cd $DOTFILE_PWD
 
-# TODO: link settings
+ln -s -f $DOTFILE_PWD/dwm/.xinitrc $HOME/.xinitrc
+ln -s -f $DOTFILE_PWD/dwm/.Xresources $HOME/.Xresources
+
+mkdir -p $HOME/.dwm
+
+ln -s -f $DOTFILE_PWD/dwm/autostart_blocking.sh $HOME/.dwm/autostart_blocking.sh
+ln -s -f $DOTFILE_PWD/dwm/autostart.sh $HOME/.dwm/autostart.sh
