e16c1252bf179f081fca8363b36340bc1583748c
Denys Lytviak <deh4er@gmail.com>
2025-06-13 21:35:48 +0200

    up

--- zsh/keys.sh
+++ zsh/keys.sh
@@ -1,12 +1,16 @@
 # Enable Vi Mode
 # bindkey -v
+# bindkey -M viins 'jk' vi-cmd-mode
+ 
+autoload -z up-line-or-beginning-search down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^P" up-line-or-beginning-search
+bindkey "^N" down-line-or-beginning-search
 
 autoload -z edit-command-line
 zle -N edit-command-line 
-
-bindkey -M viins 'jk' vi-cmd-mode
-bindkey "^P" history-beginning-search-backward
-bindkey "^N" history-beginning-search-forward
-bindkey '^[[Z' reverse-menu-complete
 bindkey '^xe' edit-command-line
 bindkey '^x^e' edit-command-line
+
+bindkey '^[[Z' reverse-menu-complete
